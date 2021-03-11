3c70ee5ec193deacf2a590cb15424b59f85317ca
Yong SangYoon <64957267+ryong9rrr@users.noreply.github.com>
2021-03-11 16:18:04 +0900

    스키마 수정 및 db파일 변경

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,9 @@
-import { getMovies } from "./db.js";
+import { getMovies, getMovie } from "./db.js";
 
 const resolvers = {
   Query: {
     movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
   },
 };
 
